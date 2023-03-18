7d21d6878cfc03a609b34e2193f8d0ba7a3ce9ea
kotyara12 <kotyara12@yandex.ru>
2023-03-18 19:15:19 +0300

    Работа с HTTP соединениями

--- http_requests/src/main.c
+++ http_requests/src/main.c
@@ -1,7 +1,15 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_http_client.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+#include "esp_log.h"
+#include "esp_heap_caps.h"
+
+#include "esp_http_client.h"
 
 #define STACK_SIZE 4096
 
@@ -14,6 +22,8 @@ void led_exec(void *pvParameters)
   esp_http_client_config_t request;
   memset(&request, 0, sizeof(request));
 
+  // Начальный URI
+  request.url = "http://open-monitoring.online/get?cid=2468&key=H9xOdR";
   // Транспорт TCP/IP
   request.transport_type = HTTP_TRANSPORT_OVER_TCP;
   // Запрос типа GET
@@ -37,14 +47,21 @@ void led_exec(void *pvParameters)
       esp_http_client_set_url(client, "http://open-monitoring.online/get?cid=2468&key=H9xOdR&p1=2&p2=hello&p3=99.99");
       // Выполняем запрос
       esp_http_client_perform(client);
+      // Закрываем соединение, если оно открыто
+      esp_http_client_close(client);
       // Анализ результатов
       int response = esp_http_client_get_status_code(client);
-      if (response != 200) {
-        ESP_LOGE(TAG, "Oops! Something went wrong: %d", response);
-      };
+      ESP_LOGI(TAG, "esp_http_client_get_status_code = %d", response);
 
-      // Пауза 1 минута
-      vTaskDelay(pdMS_TO_TICKS(60000));
+      // Выводим информацию о свободной памяти
+      double heap_total = (double)heap_caps_get_total_size(MALLOC_CAP_DEFAULT) / 1024.0;
+      double heap_free  = (double)heap_caps_get_free_size(MALLOC_CAP_DEFAULT) / 1024.0;
+      double heap_min   = (double)heap_caps_get_minimum_free_size(MALLOC_CAP_DEFAULT) / 1024.0;
+      ESP_LOGI("RAM", "Heap total: %.3f kB, free: %.3f kB (%.1f%%), minimum: %.3f kB (%.1f%%)",
+         heap_total, heap_free, 100.0*heap_free/heap_total, heap_min, 100.0*heap_min/heap_total);
+
+      // Пауза 3 минуты
+      vTaskDelay(pdMS_TO_TICKS(180000));
     };
 
     // У нас порядок простой - поел, убери за собой!
@@ -55,6 +72,13 @@ void led_exec(void *pvParameters)
 
 void app_main() 
 {
+  // Подключение к сети WiFi
+  ESP_ERROR_CHECK(nvs_flash_init());
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  ESP_ERROR_CHECK(example_connect()); 
+
+  // Запускаем задачу HTTP
   static StaticTask_t xTaskBuffer;
   static StackType_t xStack[STACK_SIZE];
 
